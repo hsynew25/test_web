a60307a6a59ee0bb23d4abb5e81457fbf8c5740d
hsy <hsynew25@gmail.com>
2020-12-29 20:46:13 +0900

    [추가] 로그인 후 헤더 프로필버튼 생성

---
+++ src/components/profileAfterLogin.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+import basicProfile from "../img/basic_profile.png";
+
+const Container = styled.div``;
+
+const Button = styled.button`
+  vertical-align: top;
+`;
+
+const Nickname = styled.span`
+  font-size: 16px;
+  vertical-align: middle;
+  display: none;
+
+  @media screen and (min-width: 768px) {
+    display: inline-block;
+    margin-right: 10px;
+  }
+`;
+
+const ImgWrap = styled.div`
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  vertical-align: middle;
+
+  @media screen and (min-width: 768px) {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+const Img = styled.img`
+  width: 100%;
+`;
+
+function ProfileAfterLogin() {
+  return (
+    <Container>
+      <Button>
+        <Nickname>나는 누구</Nickname>
+        <ImgWrap>
+          <Img src={basicProfile} />
+        </ImgWrap>
+      </Button>
+    </Container>
+  );
+}
+
+export default ProfileAfterLogin;
